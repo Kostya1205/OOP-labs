90a2e3cde61169e0ef1423449fea9298afbc10f4
Kostya Zhurovich <kostya.zhurovich1205@mail.ru>
2022-09-26 19:19:02 +0300

    t

---
+++ lab 1/lab 1/lab 1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "car.h"
+
+int main()
+{
+	Cars car = Cars("Mers");
+	std::string name;
+	int weight;
+	std::cout << "Name:";
+	getline(std::cin, name);
+	std::cout << "Weight:";
+	std::cin >> weight;
+	car.setName(name);
+	car.setWeight(weight);
+	std::cout << "Car Name:" << car.getName() << "\nWeight:" << car.getWeight();
+}
